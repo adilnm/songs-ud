40169c43e94fbd5f9cbeda28401a4b537c9f87d0
Adil Naimi <adilnm23@gmail.com>
2020-05-20 06:39:19 -0500

    create combinereducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,22 @@
-const songsReducer=()=>{
-    return [
-        {title: 'No Scrubs', duration:'4:06'},
-        {title: 'Macarena', duration:'3:06'},
-        {title: 'All Star', duration:'4:20'},
-        {title: 'I Want it That Way', duration:'1:06'}
-    ]
-}
+import { combineReducers } from "redux";
 
-const selectedSongReducer=(selectedSong=null,action)=>{
-    if (action.type==='SONG_SELECTED') {
-        return action.payload
-    }
-    return selectedSong
-}
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:06" },
+    { title: "Macarena", duration: "3:06" },
+    { title: "All Star", duration: "4:20" },
+    { title: "I Want it That Way", duration: "1:06" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
