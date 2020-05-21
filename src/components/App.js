e15ca46e282e6839c1032d9eb1d3e407181c7544
Adil Naimi <adilnm23@gmail.com>
2020-05-20 23:04:35 -0500

    display all the songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import SongList from "./SongList";
 
 export default function App() {
-    return(
-        <div>
-            App
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import {createStore} from 'redux'
-import reducers from './reducers'
+import { createStore } from "redux";
+import reducers from "./reducers";
 
-ReactDom.render(<App />, document.querySelector("#root"));
+ReactDom.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
